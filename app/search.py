6c2002a6944167ce1429ab77c043bc43dee0f2f6
Jamezzz5 <imirzian@umich.edu>
2019-07-11 15:30:24 -0400

    adjusted search for aws elasticsearch

--- app/search.py
+++ app/search.py
@@ -1,4 +1,5 @@
 from flask import current_app
+import elasticsearch.exceptions as exceptions
 
 
 def add_to_index(index, model):
@@ -7,21 +8,26 @@ def add_to_index(index, model):
     payload = {}
     for field in model.__searchable__:
         payload[field] = getattr(model, field)
-    check_index(index)
-    current_app.elasticsearch.index(index=index, id=model.id, body=payload)
+    try:
+        current_app.elasticsearch.index(index=index, id=model.id, body=payload)
+    except exceptions.AuthorizationException:
+        adjust_settings(index)
+        add_to_index(index, model)
 
 
 def remove_from_index(index, model):
     if not current_app.elasticsearch:
         return
-    check_index(index)
-    current_app.elasticsearch.delete(index=index, id=model.id)
+    try:
+        current_app.elasticsearch.delete(index=index, id=model.id)
+    except exceptions.AuthorizationException:
+        adjust_settings(index)
+        remove_from_index(index, model)
 
 
 def query_index(index, query, page, per_page):
     if not current_app.elasticsearch:
         return [], 0
-    check_index(index)
     search = current_app.elasticsearch.search(
         index=index,
         body={'query': {'multi_match': {'query': query, 'fields': ['*']}},
@@ -35,7 +41,11 @@ def check_index(index):
     blocks_settings = es_settings[index]['settings']['index']['blocks']
     for setting in ['read_only', 'write', 'read_only_allow_delete']:
         if setting in blocks_settings and blocks_settings[setting] == 'true':
-            current_app.elasticsearch.indices.put_settings(index=index, body={
-                'index': {"blocks.read_only": False,
-                          "blocks.write": False,
-                          "blocks.read_only_allow_delete": None}})
+            adjust_settings(index)
+
+
+def adjust_settings(index):
+    current_app.elasticsearch.indices.put_settings(index=index, body={
+        'index': {"blocks.read_only": False,
+                  "blocks.write": False,
+                  "blocks.read_only_allow_delete": None}})
